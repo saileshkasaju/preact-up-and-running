0f93c2bfa0b8a93eab78f0910fe50227944db55d
saileshkasaju <leosailesh@gmail.com>
2017-06-16 12:14:09 +0545

    setup simple form react style

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,28 @@
 import { h, Component } from 'preact';
-import User from './User';
-
-const users = [
-    {
-        image: 'https://avatars3.githubusercontent.com/u/16471628?v=3',
-        name: 'Sailesh Kasaju'
-    },
-    {
-        image: 'https://avatars0.githubusercontent.com/u/1643522?v=3',
-        name: 'Shane Osbourne'
-    }
-];
 
 export class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            user: null,
-            loading: true
-        }
+        this.state = { text: '' };
+        this.setText = this.setText.bind(this);
+        this.submit = this.submit.bind(this);
     }
-
-    componentDidMount() {
-        fetch(this.props.config.urls.user)
-            .then(res => res.json())
-            .then(user => {
-                this.setState({
-                    user,
-                    loading: false
-                });
-            })
-            .catch(err => console.error(err));
+    setText(e) {
+        this.setState({
+            text: e.target.value
+        });
     }
-    render({config}, {loading, user}) {
+    submit() {
+        console.log(this.state.text);
+    }
+    render() {
         return (
             <div class="app">
-                {loading
-                    ? <p>Please wait... Fetching {config.urls.user}</p>
-                    : <User name={user.name}
-                            image={user.avatar_url} />
-                }
+                <form onSubmit={this.submit} action="javascript:">
+                    <input type="text" value={this.state.text} onInput={this.setText} />
+                </form>
+                <pre><code>{JSON.stringify(this.state, null, 2)}</code></pre>
             </div>
         );
     }
