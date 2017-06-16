06519bf5f002ef4b98e7dc354ca5b1fd66605486
saileshkasaju <leosailesh@gmail.com>
2017-06-16 11:38:14 +0545

    map users form array of users

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import { h } from 'preact';
 import User from './User';
 
+const users = [
+    {
+        image: 'https://avatars3.githubusercontent.com/u/16471628?v=3',
+        name: 'Sailesh Kasaju'
+    },
+    {
+        image: 'https://avatars0.githubusercontent.com/u/1643522?v=3',
+        name: 'Shane Osbourne'
+    }
+];
+
 export function App () {
     return (
         <div class="app">
-            <User image="https://avatars3.githubusercontent.com/u/16471628?v=3&s=460"
-                  name="Sailesh Kasaju" />
-            <User image="https://avatars3.githubusercontent.com/u/16471628?v=3&s=460"
-                  name="Sailesh Kasaju" />
+            {users.map(user =>
+                <User {...user} key={user.name}/>
+            )}
         </div>
     );
 }
