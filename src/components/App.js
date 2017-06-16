8de3113a3c579676149a24b37b8750ff422b703c
saileshkasaju <leosailesh@gmail.com>
2017-06-16 13:54:28 +0545

    remove submit function

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,12 @@ import { h, Component } from 'preact';
 import linkState from 'linkstate';
 
 export class App extends Component {
-    constructor(props) {
-        super(props);
-        this.submit = this.submit.bind(this);
-    }
-    submit() {
-        console.log(this.state.text);
-    }
     render(props, { text = '' }) {
         return (
             <div class="app">
-                <form onSubmit={this.submit} action="javascript:">
-                    <input type="text"
-                           value={text}
-                           onInput={linkState(this, 'text')}/>
-                </form>
+                <input type="text"
+                       value={text}
+                       onInput={linkState(this, 'text')} />
                 <pre><code>{JSON.stringify(this.state, null, 2)}</code></pre>
             </div>
         );
