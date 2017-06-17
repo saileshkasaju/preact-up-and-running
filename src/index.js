e061473ea5c464e59d3baf2d3d7c7168b7d208ab
saileshkasaju <leosailesh@gmail.com>
2017-06-17 19:00:37 +0545

    setup redux-devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import { h, render } from 'preact';
 import { Provider } from 'preact-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
 import App from './components/App';
 
-// const store = createStore(reducer, {
-//     loading: true,
-//     user: null
-// }, applyMiddleware(thunk));
+const initialState = {
+    loading: true,
+    user: null
+};
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(thunk)));
 
 
 render(
